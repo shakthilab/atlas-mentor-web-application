3c65f09eb8fa59397ddb272da1c9ae5761e4b5d4
Shakthi2030 <shakthikumar07071981@gmail.com>
2026-06-24 20:10:57 +0530

    Prod url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   angularBaseUrl: 'http://localhost:4200',
-  apiUrl: 'http://65.0.103.210:8080/api'
-  // apiUrl: 'http://localhost:8080/api'
+  // apiUrl: 'http://65.0.103.210:8080/api'
+  serviceUrl: 'https://app.atlasmentor.com/BE'
 };
